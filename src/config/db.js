e110bbc4e839b13ebd8d945f685ba00425a183b6
APPLE <apple@APPLEs-MacBook-Pro-4.local>
2022-02-20 05:20:16 +0500

    added ssl support for db url

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,12 @@ const development = {
   use_env_variable: 'DB_DAVELOPMENT_DATABASE',
   dialect: 'postgres',
   protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 };
 
 module.exports = {
