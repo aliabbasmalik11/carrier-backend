d02740cc32501f8d6ff5bbdbaf05288b3fc30ce4
APPLE <apple@APPLEs-MacBook-Pro-4.local>
2022-02-20 05:13:32 +0500

    removed staging and production code db config

--- src/config/db.js
+++ src/config/db.js
@@ -4,19 +4,7 @@ const development = {
   dialect: 'postgres',
   protocol: 'postgres',
 };
-const staging = {
-  use_env_variable: 'DB_DAVELOPMENT_DATABASE',
-  dialect: 'postgres',
-  protocol: 'postgres',
-};
-const production = {
-  use_env_variable: 'DB_DAVELOPMENT_DATABASE',
-  dialect: 'postgres',
-  protocol: 'postgres',
-};
 
 module.exports = {
   development,
-  staging,
-  production,
 };
